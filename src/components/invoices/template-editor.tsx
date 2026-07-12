3e7ff05428a4fbc4c5117a4cc8060baf83057471
JaeJun <yyggb12@kakao.com>
2026-07-12 10:39:15 +0900

    chore(invoice): clean template editor lint

--- src/components/invoices/template-editor.tsx
+++ src/components/invoices/template-editor.tsx
@@ -9,7 +9,6 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import {
-  INVOICE_BLOCK_KEYS,
   INVOICE_COLUMN_LABELS,
   INVOICE_FONT_FAMILIES,
   INVOICE_GRID_COLUMNS,
